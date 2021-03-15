4d9461ae229adbde48c26fd86d28d8ba234915ec
FunTester <FunTester@88.com>
2021-03-15 18:26:58 +0800

    修复格式

--- src/main/groovy/com/mocofun/moco/MocoResponse.groovy
+++ src/main/groovy/com/mocofun/moco/MocoResponse.groovy
@@ -24,32 +24,32 @@ import static com.github.dreamhead.moco.util.Iterables.asIterable
 @SuppressWarnings("all")
 class MocoResponse extends MocoRequest {
 
-/**
- * 返回文本信息
- * @param content
- * @return
- */
+    /**
+     * 返回文本信息
+     * @param content
+     * @return
+     */
     static ResponseHandler textRes(String content) {
         com.github.dreamhead.moco.Moco.with content
     }
 
-/**
- * 设置json格式的返回值
- * <p>
- *  json格式转成string返回
- * </p>
- * @param json
- * @return
- */
+    /**
+     * 设置json格式的返回值
+     * <p>
+     *  json格式转成string返回
+     * </p>
+     * @param json
+     * @return
+     */
     static ResponseHandler jsonRes(JSONObject json) {
         com.github.dreamhead.moco.Moco.with json.toString()
     }
 
-/**
- * 返回对象
- * @param result
- * @return
- */
+    /**
+     * 返回对象
+     * @param result
+     * @return
+     */
     static ResponseHandler obRes(Result result) {
         com.github.dreamhead.moco.Moco.with result.toString()
     }
@@ -62,19 +62,20 @@ class MocoResponse extends MocoRequest {
         com.github.dreamhead.moco.Moco.with Result.fail(result).toString()
     }
 
-/**
- * 随机返回文本
- * @param contents
- * @return
- */
+    /**
+     * 随机返回文本
+     * @param contents
+     * @return
+     */
     static ResponseHandler cycleRes(String content, String... contents) {
         cycle content, contents
     }
-/**
- * 随机返回文本
- * @param contents
- * @return
- */
+
+    /**
+     * 随机返回文本
+     * @param contents
+     * @return
+     */
     static ResponseHandler cycleRes(JSONObject content, JSONObject... contents) {
         cycle content.toString(), (String[]) contents.toList().stream().map {x -> x.toString()}.toArray()
     }
@@ -89,195 +90,196 @@ class MocoResponse extends MocoRequest {
         cycle handler, handlers
     }
 
-/**
- * 随机返回文本，会停留在最后一个文本内容
- * @param content
- * @param contents
- * @return
- */
+    /**
+     * 随机返回文本，会停留在最后一个文本内容
+     * @param content
+     * @param contents
+     * @return
+     */
     static ResponseHandler sequenceRes(String content, String... contents) {
         com.github.dreamhead.moco.Moco.seq content, contents
     }
-/**
- * 随机返回文本，会停留在最后一个文本内容
- * @param content
- * @param contents
- * @return
- */
+
+    /**
+     * 随机返回文本，会停留在最后一个文本内容
+     * @param content
+     * @param contents
+     * @return
+     */
     static ResponseHandler sequenceRes(JSONObject content, JSONObject... contents) {
-        com.github.dreamhead.moco.Moco.seq content.toString(), (String[]) contents.toList().stream().map { x -> x.toString()}.toArray()
+        com.github.dreamhead.moco.Moco.seq content.toString(), (String[]) contents.toList().stream().map {x -> x.toString()}.toArray()
     }
 
-/**
- * 随机返回，最后会停留在最后一个handle
- * @param handler
- * @param handlers
- * @return
- */
+    /**
+     * 随机返回，最后会停留在最后一个handle
+     * @param handler
+     * @param handlers
+     * @return
+     */
     static ResponseHandler sequenceRes(ResponseHandler handler, ResponseHandler... handlers) {
         com.github.dreamhead.moco.Moco.seq handler, handlers
     }
 
-/**
- * 设置cookie，只支持一个cookie设置，因为header不允许相同的key重复
- * @param key
- * @param value
- * @return
- */
+    /**
+     * 设置cookie，只支持一个cookie设置，因为header不允许相同的key重复
+     * @param key
+     * @param value
+     * @return
+     */
     static ResponseHandler setCookie(String key, String value) {
         com.github.dreamhead.moco.Moco.cookie key, value
     }
 
-/**
- * 设置header
- * @param key
- * @param value
- * @return
- */
+    /**
+     * 设置header
+     * @param key
+     * @param value
+     * @return
+     */
     static ResponseHandler setHeader(String key, String value) {
         com.github.dreamhead.moco.Moco.header key, value
     }
 
-/**
- * 批量设置header
- * @param json
- * @return
- */
+    /**
+     * 批量设置header
+     * @param json
+     * @return
+     */
     static ResponseHandler[] setHeader(JSONObject json) {
         json.keySet().stream().map {x -> setHeader(x.toString(), json.getString(x))}.toArray() as ResponseHandler[]
     }
 
-/**
- * 设置HTTP响应码，默认200
- * @param code
- * @return
- */
+    /**
+     * 设置HTTP响应码，默认200
+     * @param code
+     * @return
+     */
     static ResponseHandler setStatus(int code) {
         com.github.dreamhead.moco.Moco.status code
     }
 
-/**
- * 代理地址
- * @param url
- * @return
- */
+    /**
+     * 代理地址
+     * @param url
+     * @return
+     */
     static ResponseHandler setProxy(String url) {
         com.github.dreamhead.moco.Moco.proxy url
     }
 
-/**
- * 延迟
- * @param duration 时间，单位：毫秒
- * @return
- */
+    /**
+     * 延迟
+     * @param duration 时间，单位：毫秒
+     * @return
+     */
     static LatencyProcedure delay(long duration) {
         com.github.dreamhead.moco.Moco.latency duration, TimeUnit.MILLISECONDS
     }
 
-/**
- * 随机
- * @param content
- * @param contents
- * @return
- */
+    /**
+     * 随机
+     * @param content
+     * @param contents
+     * @return
+     */
     static ResponseHandler random(String content, String... contents) {
         RandomHandler.newSeq(FluentIterable.from(com.github.dreamhead.moco.util.Iterables.asIterable(content, contents)).transform(com.github.dreamhead.moco.internal.ApiUtils.textToResource()))
     }
 
-/**
- * 随机返回
- * @param json
- * @param jsons
- * @return
- */
+    /**
+     * 随机返回
+     * @param json
+     * @param jsons
+     * @return
+     */
     static ResponseHandler random(JSONObject json, JSONObject... jsons) {
-        RandomHandler.newSeq(FluentIterable.from(com.github.dreamhead.moco.util.Iterables.asIterable(json.toString(), jsons.toList().stream().map { x -> x.toString()}.toArray() as String[])).transform(com.github.dreamhead.moco.internal.ApiUtils.textToResource()))
+        RandomHandler.newSeq(FluentIterable.from(com.github.dreamhead.moco.util.Iterables.asIterable(json.toString(), jsons.toList().stream().map {x -> x.toString()}.toArray() as String[])).transform(com.github.dreamhead.moco.internal.ApiUtils.textToResource()))
     }
 
-/**
- * 随机
- * @param handler
- * @param handlers
- * @return
- */
+    /**
+     * 随机
+     * @param handler
+     * @param handlers
+     * @return
+     */
     static ResponseHandler random(ResponseHandler handler, ResponseHandler... handlers) {
         RandomHandler.newSeq(com.github.dreamhead.moco.util.Iterables.asIterable(handler, handlers))
     }
 
-/**
- * 延迟响应
- * @param handler
- * @param time 时间,单位ms,存在理论BUG,不能低于50ms
- * @return
- */
+    /**
+     * 延迟响应
+     * @param handler
+     * @param time 时间,单位ms,存在理论BUG,不能低于50ms
+     * @return
+     */
     static ResponseHandler delay(ResponseHandler handler, int time) {
         DelayHandler.newSeq(handler, time)
     }
 
-/**
- * 延迟响应,默认1000ms
- * @param handler
- * @return
- */
+    /**
+     * 延迟响应,默认1000ms
+     * @param handler
+     * @return
+     */
     static ResponseHandler delay(ResponseHandler handler) {
         DelayHandler.newSeq(handler, 1000)
     }
 
-/**
- * 创建固定QPS的ResponseHandler,默认QPS=1
- * @param handler
- * @return
- */
+    /**
+     * 创建固定QPS的ResponseHandler,默认QPS=1
+     * @param handler
+     * @return
+     */
     static ResponseHandler qps(ResponseHandler handler) {
         QPSHandler.newSeq(handler, 1000)
     }
 
-/**
- * 创建固定QPS的ResponseHandler
- * @param handler
- * @param gap
- * @return
- */
+    /**
+     * 创建固定QPS的ResponseHandler
+     * @param handler
+     * @param gap
+     * @return
+     */
     static ResponseHandler qps(ResponseHandler handler, int gap) {
         QPSHandler.newSeq(handler, gap)
     }
 
-/**
- * 循环返回
- * @param content
- * @param contents
- * @return
- */
+    /**
+     * 循环返回
+     * @param content
+     * @param contents
+     * @return
+     */
     static ResponseHandler cycle(String content, String... contents) {
         CycleHandler.newSeq(FluentIterable.from(com.github.dreamhead.moco.util.Iterables.asIterable(content, contents)).transform(com.github.dreamhead.moco.internal.ApiUtils.textToResource()))
     }
 
-/**
- * 循环返回
- * @param json
- * @param jsons
- * @return
- */
+    /**
+     * 循环返回
+     * @param json
+     * @param jsons
+     * @return
+     */
     static ResponseHandler cycle(JSONObject json, JSONObject... jsons) {
-        CycleHandler.newSeq(FluentIterable.from(com.github.dreamhead.moco.util.Iterables.asIterable(json.toString(), jsons.toList().stream().map { x -> x.toString()}.toArray() as String[])).transform(com.github.dreamhead.moco.internal.ApiUtils.textToResource()))
+        CycleHandler.newSeq(FluentIterable.from(com.github.dreamhead.moco.util.Iterables.asIterable(json.toString(), jsons.toList().stream().map {x -> x.toString()}.toArray() as String[])).transform(com.github.dreamhead.moco.internal.ApiUtils.textToResource()))
     }
 
-/**
- * 循环返回
- * @param handler
- * @param handlers
- * @return
- */
+    /**
+     * 循环返回
+     * @param handler
+     * @param handlers
+     * @return
+     */
     static ResponseHandler cycle(ResponseHandler handler, ResponseHandler... handlers) {
         CycleHandler.newSeq(com.github.dreamhead.moco.util.Iterables.asIterable(handler, handlers))
     }
 
-/**
- * 限制访问频率,默认访问间隔1000ms
- * @param unlimited 不受限返回
- * @param limited 受限时候返回
- * @return
- */
+    /**
+     * 限制访问频率,默认访问间隔1000ms
+     * @param unlimited 不受限返回
+     * @param limited 受限时候返回
+     * @return
+     */
     static ResponseHandler limit(String unlimited, String limited) {
         limit textRes(limited), textRes(limited)
     }
@@ -291,14 +293,14 @@ class MocoResponse extends MocoRequest {
     }
 
 
-/**
- *
- * 限制访问频率
- * @param unlimited 不受限返回
- * @param limited 受限时候返回
- * @param interval 访问间隔
- * @return
- */
+    /**
+     *
+     * 限制访问频率
+     * @param unlimited 不受限返回
+     * @param limited 受限时候返回
+     * @param interval 访问间隔
+     * @return
+     */
     static ResponseHandler limit(String unlimited, String limited, int interval) {
         limit textRes(unlimited), textRes(limited), interval
     }
