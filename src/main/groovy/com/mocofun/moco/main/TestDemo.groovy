b7a61cb35b34e109dee93157f9ee176d1eb73077
FunTester <FunTester@88.com>
2021-05-14 10:27:56 +0800

    增加接口

--- src/main/groovy/com/mocofun/moco/main/TestDemo.groovy
+++ src/main/groovy/com/mocofun/moco/main/TestDemo.groovy
@@ -6,6 +6,7 @@ class TestDemo extends MocoServer{
 
     static void main(String[] args) {
         def server = getServerNoLog(12345)
+        server.post(urlStartsWith("/post")).response(textRes("FunTester"))
         server.get(urlStartsWith("/test/qps")).response(qps(textRes("恭喜到达QPS!"),50))
         server.response("到底了,没啥响应了")
         def run = run(server)
