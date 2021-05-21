f508b366d304fd26f00a2a77ad38a122a985f2a5
FunTester <FunTester@88.com>
2021-05-21 10:38:43 +0800

    简化功能

--- src/main/groovy/com/mocofun/moco/main/Share.groovy
+++ src/main/groovy/com/mocofun/moco/main/Share.groovy
@@ -5,8 +5,7 @@ import com.mocofun.moco.MocoServer
 class Share extends MocoServer{
 
     static void main(String[] args) {
-        def server = getServerNoLog(12345)
-        server.post(urlStartsWith("/post")).response(textRes("FunTester"))
+        def server = getServer(12345)
         server.get(urlStartsWith("/test/qps")).response(qps(textRes("恭喜到达QPS!"),50))
         server.response("到底了,没啥响应了")
         def run = run(server)
